30e28fdf3a572a63853cd78616724d3720e1b782
claire thomas <claire_thomas@hotmail.fr>
2018-08-15 17:56:32 +0200

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Le rouge bar',
+    address:      '15 rue de la monnaie Lille',
+    phone_number: '03 20 55 75 43 ',
+    category:     'french'
+  },
+  {
+    name:         'Le lanna',
+    address:      '28 Place Abbé Bonpain Bondues',
+    phone_number: '03 20 55 75 43 ',
+    category:     'chinese'
+  },
+  {
+    name:         'La bottega',
+    address:      '8 rue au Péterinck Lille',
+    phone_number: '03 20 98 41 73 ',
+    category:     'italian'
+  },
+  {
+    name:         'Sakura',
+    address:      '16 rue thiers Lille',
+    phone_number: '03 20 51 85 49 ',
+    category:     'japanese'
+  },
+  {
+    name:         'La ferme de Balthazar',
+    address:      '4 Drève des Préaches Mouscron',
+    phone_number: '32 56 34 66 47 ',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
